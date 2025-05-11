[fix] align with mlir-aie release v1.0, resolve 'Overflow of program memory' when using matmul external kernel

tests/dataflow/aie/experimental/test_matmul.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import allo
-from allo.ir.types import int32
+from allo.ir.types import int16, int32
 import allo.dataflow as df
 import numpy as np
 from allo.memory import Layout
@@ -52,6 +52,48 @@ def _test_gemm_2D():
     print("PASSED!")
 
 
+def _test_gemm_1D_i16_i16():
+    Ty = int16
+    M, N, K = 16, 16, 16
+    P0 = 2
+
+    @df.region()
+    def top():
+        @df.kernel(mapping=[P0])
+        def gemm(A: Ty[M, K] @ LyA, B: Ty[K, N], C: Ty[M, N] @ LyA):
+            C[:, :] = allo.matmul(A, B)
+
+    mod = df.build(top, target="aie-mlir")
+    A = np.random.randint(0, 64, (M, K)).astype(np.int16)
+    B = np.random.randint(0, 64, (K, N)).astype(np.int16)
+    C = np.zeros((M, N)).astype(np.int16)
+    mod(A, B, C)
+    np.testing.assert_allclose(C, A @ B, atol=1e-5)
+    print("PASSED!")
+
+
+def _test_gemm_2D_i16_i32():
+    TyI, TyO = int16, int32
+    M, N, K = 32, 32, 32
+    P0, P1 = 4, 4
+
+    @df.region()
+    def top():
+        @df.kernel(mapping=[P0, P1])
+        def gemm(A: TyI[M, K] @ LyA, B: TyI[K, N] @ LyB, C: TyO[M, N] @ LyC):
+            C[:, :] = allo.matmul(A, B)
+
+    mod = df.build(top, target="aie-mlir")
+    A = np.random.randint(0, 64, (M, K)).astype(np.int16)
+    B = np.random.randint(0, 64, (K, N)).astype(np.int16)
+    C = np.zeros((M, N)).astype(np.int32)
+    mod(A, B, C)
+    np.testing.assert_allclose(C, A @ B, atol=1e-5)
+    print("PASSED!")
+
+
 if __name__ == "__main__":
     _test_gemm_1D()
     _test_gemm_2D()
+    _test_gemm_1D_i16_i16()
+    _test_gemm_2D_i16_i32()