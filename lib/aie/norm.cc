3f897c52ef151a43e801e965620dc59836428bf6
Fangtangtang <2167295472@qq.com>
2025-05-15 22:21:33 -0400

    ckpt

--- lib/aie/norm.cc
+++ lib/aie/norm.cc
@@ -98,4 +98,11 @@ void layer_norm(float *A_in, float *B_in, float *C_out) {
   layer_norm_single_batch_no_bias<float, float, 4, 512>(A_in, B_in, C_out);
 }
 
+// new version for `ExternalModule`
+extern "C" {
+void rms_norm(float A_in[4][512], float B_in[512], float C_out[4][512]) {
+  rms_norm_single_batch<float, float, 4, 512>(&A_in[0][0], B_in, &C_out[0][0]);
+}
+}
+
 } // extern "C"
