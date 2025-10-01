4e2ee49fb57940ac19ac0555d74852ceb634d3c4
Fangtangtang <2167295472@qq.com>
2025-10-01 15:22:27 -0400

    update

--- tests/dataflow/aie/run.sh
+++ tests/dataflow/aie/run.sh
@@ -9,19 +9,24 @@ PYTHON=python3
 FILES=(
     test_cannon.py                   
     test_gemm_temporal_reduction.py  
+    test_low_bit_gemm.py
     test_mapping_basic.py       
     test_mapping_gemm.py        
-    test_mapping_large_gemm.py  
+    # test_mapping_large_gemm.py  
+    test_mapping_multi_gemm.py  
     test_mapping_tp.py  
     test_matrix.py      
+    test_meta_for.py      
+    test_multi_output.py      
     test_norm.py        
     test_pingpong_gemm.py      
     test_producer_consumer.py  
     test_summa.py
     test_tp.py                   
-    test_vector.py
-    test_trace_data_transfer.py
     # test_trace_conv.py
+    test_trace_data_transfer.py
+    test_vector.py
+    # test_vliw.py
 )
 
 echo "Running ${#FILES[@]} Python scripts..."
