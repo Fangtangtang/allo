ed6dde67155c4cd127d5baba9b3f133a0fc6bd45
Fangtangtang <2167295472@qq.com>
2025-10-30 17:47:00 -0400

    [test] add more test cases

--- tests/test_vitis.py
+++ tests/test_vitis.py
@@ -3,96 +3,125 @@
 
 import numpy as np
 import allo
-from allo.ir.types import uint64, uint256, int32, float32
+from allo.ir.types import uint64, uint256, int32, float32, int16, bool
 from allo.utils import get_np_struct_type
 import allo.dataflow as df
 from allo.backend import hls
 
 
-def pack_i32_to_i256(arr):
-    arr = np.array(arr, dtype=np.uint32)
-    n = len(arr)
-    assert n % 8 == 0, "Length must be a multiple of 8"
+# ##############################################################
+# Test basic
+# ##############################################################
+def test_grid_for_gemm():
+    # from `test_builder.py`, with return value
 
-    packed = []
-    for i in range(0, n, 8):
-        parts = []
-        for j in range(0, 8, 2):
-            low32 = int(arr[i + j])
-            high32 = int(arr[i + j + 1])
-            val64 = (high32 << 32) | low32
-            parts.append(np.uint64(val64))
-        packed.append(parts)
-    print(packed)
-    return np.array(packed, dtype=np.uint64)
+    # This test is to make sure the whole flow works properly.
+    def gemm(A: int32[32, 32], B: int32[32, 32]) -> int32[32, 32]:
+        C: int32[32, 32] = 0
+        # Use grid_for with name annotation
+        for i, j, k in allo.grid(32, 32, 32, name="C"):
+            C[i, j] += A[i, k] * B[k, j]
+        return C
 
-
-def unpack_i256_to_i32(packed):
-    packed = np.array(packed, dtype=np.int64)
-    result = []
-    mask = (1 << 32) - 1
-    for j in range(4):
-        val64 = int(packed[j])
-        low32 = np.uint32(val64 & mask)
-        high32 = np.uint32((val64 >> 32) & mask)
-        result.extend([low32, high32])
-    return np.array(result, dtype=np.uint32)
-
-
-def test_vadd():
-    VLEN = 256
-    ENTRY_SIZE = 64
-    ELEN = 32
-
-    @df.region()
-    def top():
-        @df.kernel(mapping=[1])
-        def VEC(
-            A: uint64[VLEN // ENTRY_SIZE],
-            B: uint64[VLEN // ENTRY_SIZE],
-            C: uint64[VLEN // ENTRY_SIZE],
-        ):
-            for i, j in allo.grid(
-                VLEN // ENTRY_SIZE, ENTRY_SIZE // ELEN, name="vec_nest"
-            ):
-                C[i][j * ELEN : (j + 1) * ELEN] = (
-                    A[i][j * ELEN : (j + 1) * ELEN] + B[i][j * ELEN : (j + 1) * ELEN]
-                )
-
-    A = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
-    B = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
-    packed_A = pack_i32_to_i256(A)[0]
-    packed_B = pack_i32_to_i256(B)[0]
-    packed_C = np.zeros((VLEN // ENTRY_SIZE,)).astype(np.uint64)
-
-    mod = df.build(top, target="simulator")
-    mod(packed_A, packed_B, packed_C)
-    C_ = unpack_i256_to_i32(packed_C)
-    np.testing.assert_allclose(A + B, C_, rtol=1e-5, atol=1e-5)
-    print("PASSED!")
-
-    s = df.customize(top)
-    # unroll the lanes
-    nest_loop_i = s.get_loops("VEC_0")["vec_nest"]["i"]
-    s.unroll(nest_loop_i)
-    nest_loop_j = s.get_loops("VEC_0")["vec_nest"]["j"]
-    s.unroll(nest_loop_j)
+    # 1. Create customization
+    s = allo.customize(gemm)
     print(s.module)
 
+    # 2. Apply transformations and make sure each step the module can be printed
+    s.split("i", 8)
+    print(s.module)
+    s.split("j", 8)
+    print(s.module)
+    s.reorder("i.outer", "j.outer", "i.inner", "j.inner")
+    print(s.module)
+    # Make sure the generated loops are correct and ordered
+    loops = s.get_loops()
+    expected = ["i.outer", "j.outer", "i.inner", "j.inner", "k"]
+    assert expected == list(loops.C.loops.keys())
+
+    # 5. HLS CSIM
     if hls.is_available("vitis_hls"):
-        print("Starting Test...")
-        mod = s.build(
+        np_A = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
+        np_B = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
+        np_C = np.matmul(np_A, np_B)
+        hls_mod = s.build(
             target="vitis_hls",
             mode="sw_emu",
-            project=f"vec_sw_emu.prj",
-            wrap_io=False,
+            project=f"test_grid_for_gemm.prj",
         )
-        mod(packed_A, packed_B, packed_C)
-        C_ = unpack_i256_to_i32(packed_C)
-        np.testing.assert_allclose(A + B, C_, rtol=1e-5, atol=1e-5)
-        print("Passed Test!")
+        sw_out = np.zeros((32, 32), dtype=np.int32)
+        hls_mod(np_A, np_B, sw_out)
+        np.testing.assert_allclose(sw_out, np_C, atol=1e-3)
+        print("Passed HLS test!")
 
 
+def test_vitis_gemm_template_int32():
+    # from `test_vhls.py`
+    def gemm[T, M, N, K](A: "T[M, K]", B: "T[K, N]") -> "T[M, N]":
+        C: T[M, N] = 0
+        for i, j, k in allo.grid(M, N, K, name="C"):
+            C[i, j] += A[i, k] * B[k, j]
+        return C
+
+    s = allo.customize(gemm, instantiate=[int32, 32, 32, 32])
+    if hls.is_available("vitis_hls"):
+        mod = s.build(
+            target="vitis_hls", mode="sw_emu", project=f"gemm_vitis_{int32}.prj"
+        )
+        np_A = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
+        np_B = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
+        np_C = np.matmul(np_A, np_B)
+        np_C_allo = np.zeros((32, 32), dtype=np.int32)
+        mod(np_A, np_B, np_C_allo)
+        np.testing.assert_allclose(np_C, np_C_allo, rtol=1e-4)
+        print("Passed!")
+
+
+def test_vitis_gemm_template_float32():
+    # from `test_vhls.py`
+    def gemm[T, M, N, K](A: "T[M, K]", B: "T[K, N]") -> "T[M, N]":
+        C: T[M, N] = 0
+        for i, j, k in allo.grid(M, N, K, name="C"):
+            C[i, j] += A[i, k] * B[k, j]
+        return C
+
+    s = allo.customize(gemm, instantiate=[float32, 64, 64, 64])
+    if hls.is_available("vitis_hls"):
+        mod = s.build(
+            target="vitis_hls", mode="sw_emu", project=f"gemm_vitis_{float32}.prj"
+        )
+        np_A = np.random.random(size=(64, 64)).astype(np.float32)
+        np_B = np.random.random(size=(64, 64)).astype(np.float32)
+        np_C = np.matmul(np_A, np_B)
+        np_C_allo = np.zeros((64, 64), dtype=np.float32)
+        mod(np_A, np_B, np_C_allo)
+        np.testing.assert_allclose(np_C, np_C_allo, rtol=1e-4)
+        print("Passed!")
+
+
+def test_vitis_io_stream():
+    # from `test_vhls.py`
+    def foo(A: int32[32, 32], B: int32[32, 32]):
+        pass
+
+    def top(A: int32[32, 32]) -> int32[32, 32]:
+        B: int32[32, 32]
+        foo(A, B)
+        return B
+
+    s = allo.customize(top)
+    s.dataflow("top")
+    if hls.is_available("vitis_hls"):
+        hls_mod = s.build(target="vitis_hls", mode="sw_emu", project="test_io.prj")
+        print(s.module)
+        np_A = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
+        np_B = np.zeros((32, 32), dtype=np.int32)
+        hls_mod(np_A, np_B)
+
+
+# ##############################################################
+# Test large bitwidth
+# ##############################################################
 def test_vadd_adv():
     VLEN = 256
     ELEN = 32
@@ -148,50 +177,10 @@ def test_vadd_adv():
         print("Passed Test!")
 
 
-def test_grid_for_gemm():
-    # from `test_builder.py`
-
-    # This test is to make sure the whole flow works properly.
-    def gemm(A: int32[32, 32], B: int32[32, 32]) -> int32[32, 32]:
-        C: int32[32, 32] = 0
-        # Use grid_for with name annotation
-        for i, j, k in allo.grid(32, 32, 32, name="C"):
-            C[i, j] += A[i, k] * B[k, j]
-        return C
-
-    # 1. Create customization
-    s = allo.customize(gemm)
-    print(s.module)
-
-    # 2. Apply transformations and make sure each step the module can be printed
-    s.split("i", 8)
-    print(s.module)
-    s.split("j", 8)
-    print(s.module)
-    s.reorder("i.outer", "j.outer", "i.inner", "j.inner")
-    print(s.module)
-    # Make sure the generated loops are correct and ordered
-    loops = s.get_loops()
-    expected = ["i.outer", "j.outer", "i.inner", "j.inner", "k"]
-    assert expected == list(loops.C.loops.keys())
-
-    # 5. HLS CSIM
-    if hls.is_available("vitis_hls"):
-        np_A = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
-        np_B = np.random.randint(0, 10, size=(32, 32)).astype(np.int32)
-        np_C = np.matmul(np_A, np_B)
-        hls_mod = s.build(
-            target="vitis_hls",
-            mode="sw_emu",
-            project=f"test_grid_for_gemm.prj",
-        )
-        sw_out = np.zeros((32, 32), dtype=np.int32)
-        hls_mod(np_A, np_B, sw_out)
-        np.testing.assert_allclose(sw_out, np_C, atol=1e-3)
-        print("Passed HLS test!")
-
-
 if __name__ == "__main__":
     test_vadd()
     test_vadd_adv()
     test_grid_for_gemm()
+    test_vitis_gemm_template_int32()
+    test_vitis_gemm_template_float32()
+    test_vitis_io_stream()
