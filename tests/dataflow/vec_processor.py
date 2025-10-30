ac1678f6bdc087d23d8bf348afed1e156c039e9e
Fangtangtang <2167295472@qq.com>
2025-10-29 23:24:22 -0400

    ckpt

--- tests/dataflow/vec_processor.py
+++ tests/dataflow/vec_processor.py
@@ -6,7 +6,7 @@ import allo
 from allo.ir.types import UInt, uint2, int32
 import allo.dataflow as df
 
-VLEN = 256
+VLEN = 128
 ELEN = 32
 
 import sys
@@ -27,24 +27,24 @@ import sys
 # assert np.array_equal(golden, np_B)
 # sys.exit(0)
 
-def pack_i32_to_i256(arr):
+def pack_i32_to_i128(arr):
     arr = np.array(arr, dtype=np.uint32)
     n = len(arr)
-    assert n % 8 == 0
+    assert n % 4 == 0
 
     packed = []
-    for i in range(0, n, 8):
+    for i in range(0, n, 4):
         value = 0
-        for j in range(8):
+        for j in range(4):
             value |= int(arr[i + j]) << (32 * j)
         packed.append(value)
     return np.array(packed, dtype=object)
 
 
-def unpack_i256_to_i32(value):
+def unpack_i128_to_i32(value):
     result = []
     mask = (1 << 32) - 1
-    for i in range(8):
+    for i in range(4):
         part = (value >> (32 * i)) & mask
         result.append(np.int32(part))
     return np.array(result, dtype=np.int32)
@@ -53,25 +53,22 @@ def unpack_i256_to_i32(value):
 @df.region()
 def top():
     @df.kernel(mapping=[1])
-    def gemm0(A: UInt(VLEN), B: UInt(VLEN), C: UInt(VLEN)[1]):
+    def gemm0(A: UInt(VLEN)[1], B: UInt(VLEN)[1], C: UInt(VLEN)[1]):
         for i in range(VLEN // ELEN):
             C[0][i * ELEN : (i + 1) * ELEN] = (
-                A[i * ELEN : (i + 1) * ELEN] + B[i * ELEN : (i + 1) * ELEN]
+                A[0][i * ELEN : (i + 1) * ELEN] + B[0][i * ELEN : (i + 1) * ELEN]
             )
 
 
 A = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.int32)
 B = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.int32)
-packed_A = pack_i32_to_i256(A)[0]
-packed_B = pack_i32_to_i256(B)[0]
-packed_C = pack_i32_to_i256(B)[0]
+packed_A = pack_i32_to_i128(A)
+packed_B = pack_i32_to_i128(B)
+packed_C = pack_i32_to_i128(B)
 
 # mod = df.build(top)
-# mod = df.build(top, target="simulator")
-s = allo.customize(top)
-print(s.module)
-mod  = s.build()
+mod = df.build(top, target="simulator")
 mod(packed_A, packed_B, packed_C)
-C_ = unpack_i256_to_i32(packed_C)
+C_ = unpack_i128_to_i32(packed_C[0])
 print(C_)
 print(A + B, C_)
