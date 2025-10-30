11d05055f22e2551e341d656e24b478a4b469e26
Fangtangtang <2167295472@qq.com>
2025-10-30 15:06:04 -0400

    ckpt

--- tests/dataflow/host_test.py
+++ tests/dataflow/host_test.py
@@ -82,69 +82,18 @@ def test_vadd():
     print(s.module)
 
     if hls.is_available("vitis_hls"):
-        print("Starting hw Test...")
-        modhw = s.build(
+        print("Starting Test...")
+        mod = s.build(
             target="vitis_hls",
-            mode="hw",
-            project=f"vec_hw.prj",
+            mode="sw_emu",
+            project=f"vec_sw_emu.prj",
             wrap_io=False,
         )
-        modhw(packed_A, packed_B, packed_C)
+        mod(packed_A, packed_B, packed_C)
         C_ = unpack_i256_to_i32(packed_C)
         np.testing.assert_allclose(A + B, C_, rtol=1e-5, atol=1e-5)
-        print("Passed hw Test!")
-
-
-def test_vadd_adv():
-
-    @df.region()
-    def top():
-        @df.kernel(mapping=[1])
-        def VEC(
-            A: uint256[1],
-            B: uint256[1],
-            C: uint256[1],
-        ):
-            for i in allo.grid(VLEN // ELEN, name="vec_nest"):
-                C[0][i * ELEN : (i + 1) * ELEN] = (
-                    A[0][i * ELEN : (i + 1) * ELEN] + B[0][i * ELEN : (i + 1) * ELEN]
-                )
-
-    A = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
-    B = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
-    C = np.zeros(
-        VLEN // ELEN,
-    ).astype(np.uint32)
-    packed_A = np.ascontiguousarray(A).view(np_256)
-    packed_B = np.ascontiguousarray(B).view(np_256)
-    packed_C = np.ascontiguousarray(C).view(np_256)
-
-    mod = df.build(top, target="simulator")
-    mod(packed_A, packed_B, packed_C)
-    unpacked_C = packed_C.view(np.uint32)
-    np.testing.assert_allclose(A + B, unpacked_C, rtol=1e-5, atol=1e-5)
-    print("PASSED!")
-
-    s = df.customize(top)
-    # unroll the lanes
-    nest_loop_i = s.get_loops("VEC_0")["vec_nest"]["i"]
-    s.unroll(nest_loop_i)
-    print(s.module)
-
-    # if hls.is_available("vitis_hls"):
-    #     print("Starting hw Test...")
-    #     modhw = s.build(
-    #         target="vitis_hls",
-    #         mode="hw",
-    #         project=f"vec_hw.prj",
-    #         wrap_io=False,
-    #     )
-    #     modhw(packed_A, packed_B, packed_C)
-    #     unpacked_C = packed_C.view(np.uint32)
-    #     np.testing.assert_allclose(A + B, unpacked_C, rtol=1e-5, atol=1e-5)
-    #     print("Passed hw Test!")
+        print("Passed Test!")
 
 
 if __name__ == "__main__":
     test_vadd()
-    test_vadd_adv()
