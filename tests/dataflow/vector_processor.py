c4ce8b916db1ef80f32fb9ea999c8c454bc6b40b
Fangtangtang <2167295472@qq.com>
2025-10-31 13:27:12 -0400

    [feat] vadd

--- tests/dataflow/vector_processor.py
+++ tests/dataflow/vector_processor.py
@@ -2,55 +2,66 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import allo
-from allo.ir.types import UInt
+from allo.ir.types import uint256
 import allo.dataflow as df
+from allo.utils import get_np_struct_type
+from allo.backend import hls
 
 VLEN = 256
 ELEN = 32
 
 import numpy as np
 
-
-def pack_i32_to_i256(arr):
-    arr = np.array(arr, dtype=np.uint32)
-    n = len(arr)
-    assert n % 8 == 0
-
-    packed = []
-    for i in range(0, n, 8):
-        value = 0
-        for j in range(8):
-            value |= int(arr[i + j]) << (32 * j)
-        packed.append(value)
-    return np.array(packed, dtype=object)
+np_256 = get_np_struct_type(VLEN)
 
 
-def unpack_i256_to_i32(value):
-    result = []
-    mask = (1 << 32) - 1
-    for i in range(8):
-        part = (value >> (32 * i)) & mask
-        result.append(np.int32(part))
-    return np.array(result, dtype=np.int32)
+def test_vadd():
+    @df.region()
+    def top():
+        @df.kernel(mapping=[1])
+        def VEC(
+            A: uint256[1],
+            B: uint256[1],
+            C: uint256[1],
+        ):
+            for i in allo.grid(VLEN // ELEN, name="vec_nest"):
+                C[0][i * ELEN : (i + 1) * ELEN] = (
+                    A[0][i * ELEN : (i + 1) * ELEN] + B[0][i * ELEN : (i + 1) * ELEN]
+                )
+
+    A = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
+    B = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.uint32)
+    C = np.zeros(VLEN // ELEN).astype(np.uint32)
+    packed_A = np.ascontiguousarray(A).view(np_256)
+    packed_B = np.ascontiguousarray(B).view(np_256)
+    packed_C = np.ascontiguousarray(C).view(np_256)
+
+    mod = df.build(top, target="simulator")
+    mod(packed_A, packed_B, packed_C)
+    unpacked_C = packed_C.view(np.uint32)
+    np.testing.assert_allclose(A + B, unpacked_C, rtol=1e-5, atol=1e-5)
+    print("PASSED!")
+
+    s = df.customize(top)
+    # unroll the lanes
+    nest_loop_i = s.get_loops("VEC_0")["vec_nest"]["i"]
+    s.unroll(nest_loop_i)
+    print(s.module)
+
+    if hls.is_available("vitis_hls"):
+        print("Starting Test...")
+        mod = s.build(
+            target="vitis_hls",
+            mode="sw_emu",
+            project=f"vec_adv_sw_emu.prj",
+            wrap_io=False,
+        )
+        mod(packed_A, packed_B, packed_C)
+        unpacked_C = packed_C.view(np.uint32)
+        np.testing.assert_allclose(A + B, unpacked_C, rtol=1e-5, atol=1e-5)
+        print(unpacked_C)
+        print("Passed Test!")
 
 
-@df.region()
-def top():
-    @df.kernel(mapping=[1])
-    def gemm0(A: UInt(VLEN), B: UInt(VLEN), C: UInt(VLEN)):
-        with allo.meta_for(VLEN // ELEN) as i:
-            C[i * ELEN : (i + 1) * ELEN] = (
-                A[i * ELEN : (i + 1) * ELEN] + B[i * ELEN : (i + 1) * ELEN]
-            )
-
-
-A = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.int32)
-B = np.random.randint(0, 64, (VLEN // ELEN,)).astype(np.int32)
-packed_A = pack_i32_to_i256(A)[0]
-packed_B = pack_i32_to_i256(B)[0]
-packed_C = pack_i32_to_i256(B)[0]
-
-mod = df.build(top, target="simulator")
-mod(packed_A, packed_B, packed_C)
-C_ = unpack_i256_to_i32(packed_C)
-print(A + B, C_)
+if __name__ == "__main__":
+    test_vadd()
