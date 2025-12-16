c9e8b43a07c7bc0780a66f87f56fc550e5476d45
Fangtangtang <2167295472@qq.com>
2025-12-16 20:58:00 +0000

    [test] execute from project test cases

--- tests/dataflow/air/test_external_kernel_from_project.py
+++ tests/dataflow/air/test_external_kernel_from_project.py
@@ -6,19 +6,18 @@ from allo.backend.aie import is_available
 from allo.backend import air
 
 
-def _test_vector_vector_add():
-    M = 1024
+def _test_external_kernel():
+    M = 4096
 
-    A = np.random.random(M).astype(np.float32)
-    B = np.random.random(M).astype(np.float32)
+    A = np.random.random(M).astype(np.uint8)
     if is_available():
-        C = np.zeros(M).astype(np.float32)
-        air._call_prj("vadd_prj", [2], A, B, C)
-        np.testing.assert_allclose(C, A + B, rtol=1e-5)
+        B = np.zeros(M).astype(np.uint8)
+        air._call_prj("external_prj", [1], A, B)
+        np.testing.assert_allclose(A, B, rtol=1e-5)
         print("PASSED!")
     else:
         print("MLIR_AIE_INSTALL_DIR unset. Skipping AIE backend test.")
 
 
 if __name__ == "__main__":
-    _test_vector_vector_add()
+    _test_external_kernel()
