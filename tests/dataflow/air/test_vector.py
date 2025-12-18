702d3e143a7d096625117e0462e79dd39255b839
Fangtangtang <2167295472@qq.com>
2025-12-18 12:57:25 +0000

    [testcast] add more testing for single tile (though not very meaningful)

--- tests/dataflow/air/test_vector.py
+++ tests/dataflow/air/test_vector.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import allo
-from allo.ir.types import int32, float32
+from allo.ir.types import int32, float32, bfloat16
 import allo.dataflow as df
 import numpy as np
 from allo.backend.aie import is_available
@@ -72,7 +72,53 @@ def _test_vector_vector_add():
         print("MLIR_AIE_INSTALL_DIR unset. Skipping AIE backend test.")
 
 
+def _test_vector_scalar_mul():
+    Ty = float32
+    M = 512
+
+    @df.region()
+    def top():
+        @df.kernel(mapping=[1])
+        def core(A: Ty[M], B: Ty[M]):
+            B[:] = allo.mul(A, 2)
+
+    A = np.random.random(M).astype(np.float32)
+    if is_available():
+        mod = df.build(top, target="air")
+        B = np.zeros(M).astype(np.float32)
+        mod(A, B)
+        np.testing.assert_allclose(B, A * 2, rtol=1e-5)
+        print("PASSED!")
+    else:
+        print("MLIR_AIE_INSTALL_DIR unset. Skipping AIE backend test.")
+
+
+def _test_vector_vector_mul():
+    Ty = float32
+    M = 1024
+
+    @df.region()
+    def top():
+        @df.kernel(mapping=[1])
+        def core(A: Ty[M], B: Ty[M], C: Ty[M]):
+            C[:] = allo.mul(A, B)
+
+    A = np.random.random(M).astype(np.float32)
+    B = np.random.random(M).astype(np.float32)
+
+    if is_available():
+        mod = df.build(top, target="air")
+        C = np.zeros(M).astype(np.float32)
+        mod(A, B, C)
+        np.testing.assert_allclose(C, A * B, rtol=1e-5)
+        print("PASSED!")
+    else:
+        print("MLIR_AIE_INSTALL_DIR unset. Skipping AIE backend test.")
+
+
 if __name__ == "__main__":
     _test_passthrough()
     _test_vector_vector_add()
     _test_vector_scalar_add()
+    _test_vector_scalar_mul()
+    _test_vector_vector_mul()
