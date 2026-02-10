1fdef8473e4d06adf294ef3ba7d5b3a43829e5de
Fangtangtang <2167295472@qq.com>
2026-02-10 10:36:44 -0500

    [feat] spmw decorators

---
+++ allo/spmw.py
@@ -0,0 +1,51 @@
+# Copyright Allo authors. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import ast
+from pathlib import Path
+
+MODULE_CACHE = {}
+
+
+def get_module(filename):
+    if filename not in MODULE_CACHE:
+        src = Path(filename).read_text()
+        MODULE_CACHE[filename] = (src, ast.parse(src))
+    return MODULE_CACHE[filename]
+
+
+def find_function_ast(fn):
+    filename = fn.__code__.co_filename
+    lineno = fn.__code__.co_firstlineno + 1
+    src, tree = get_module(filename)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef) and node.lineno == lineno:
+            return src, node
+    raise RuntimeError("Function AST not found")
+
+
+def kernel(fn):
+    src, node = find_function_ast(fn)
+    fn._source = ast.get_source_segment(src, node)
+    fn._ast = node
+    return fn
+
+
+def unit():
+    def decorator(fn):
+        return fn
+
+    return decorator
+
+def work(*, mapping: list[int], args=None):
+    def decorator(fn):
+        src, node = find_function_ast(fn)
+        fn._df_meta = {
+            "mapping": mapping,
+            "args": args,
+        }
+        fn._source = ast.get_source_segment(src, node)
+        fn._ast = node
+
+        return fn 
+    return decorator
