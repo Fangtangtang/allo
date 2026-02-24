8a67a590d9d0b648814d00ce3cef7066373f0dd8
Fangtangtang <2167295472@qq.com>
2026-02-24 07:21:42 -0500

    [feat] lower memref.copy for hls backend

--- mlir/lib/Bindings/AlloModule.cpp
+++ mlir/lib/Bindings/AlloModule.cpp
@@ -188,6 +188,11 @@ static bool lowerTransformLayoutOps(MlirModule &mlir_mod) {
   return applyLowerTransformLayoutOps(mod);
 }
 
+static bool lowerMemCopyOps(MlirModule &mlir_mod) {
+  auto mod = unwrap(mlir_mod);
+  return applyLowerMemCopyOps(mod);
+}
+
 static bool legalizeCast(MlirModule &mlir_mod) {
   auto mod = unwrap(mlir_mod);
   return applyLegalizeCast(mod);
@@ -358,6 +363,7 @@ NB_MODULE(_allo, m) {
   allo_m.def("lower_composite_type", &lowerCompositeType);
   allo_m.def("lower_bit_ops", &lowerBitOps);
   allo_m.def("lower_transform_layout_ops", &lowerTransformLayoutOps);
+  allo_m.def("lower_memcopy_ops", &lowerMemCopyOps);
   allo_m.def("legalize_cast", &legalizeCast);
   allo_m.def("remove_stride_map", &removeStrideMap);
   allo_m.def("lower_print_ops", &lowerPrintOps);
